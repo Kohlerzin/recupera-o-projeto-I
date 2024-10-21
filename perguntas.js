a08a7a622a5b9ff079375828ce9c1083a4f7141a
Kohlerzin <161029054+Kohlerzin@users.noreply.github.com>
2024-10-21 08:02:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quizz',
+    'O que é Ferrari?',
+    'é uma fabricante italiana de carros esportivos de luxo com sede em Maranello.'
+)
+
+criaCartao(
+    'Quizz',
+    'Quem foi Enzo Ferrari?',
+    'Enzo Anselmo Giuseppe Maria Ferrari (1898-1988) foi o fundador da Scuderia Ferrari e da fábrica de automóveis Ferrari.'
+)
+
+criaCartao(
+    'Quizz',
+    'Qual o propósito da Ferrari?',
+    'Tem o propósito de dar apoio a pilotos que competiam em corridas automóveis com viaturas Alfa Romeo, também italianas.'
+)
+
+criaCartao(
+    'Quizz',
+    'Sobre a Ferrari',
+    'Fundada por Enzo Ferrari em 1939 na divisão de corridas da Alfa Romeo com o nome Auto Avio Costruzioni, a empresa construiu seu primeiro carro em 1940. No entanto, o início da empresa como fabricante de automóveis é geralmente reconhecido em 1947, quando o primeiro carro com o nome Ferrari foi concluído.'
+)
